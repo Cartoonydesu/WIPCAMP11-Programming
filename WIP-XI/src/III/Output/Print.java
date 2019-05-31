4e963484a171d1d6d7e04a0d678472440f79f74a
Cartoonydesu <toon.pi45@gmail.com>
2019-05-31 08:43:58 +0700

    Add project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,22 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("วรั๊ยยยยยย\nตัยล้าวววววว");
+        
+        System.out.println("อยู่บรรทัดนี้นะ");
+        
+        System.out.print("หลังจากนี้ขึ่นใหม่ \n");
+        
+        System.out.print("อยู่บรรทัดใหม่ละ");
+        
+    }
+}
---
+++ WIP-XI/src/V/Selection/_2_IF_ELSE.java
@@ -0,0 +1,24 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package V.Selection;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _2_IF_ELSE {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+        
+        if(a < 12){
+            System.out.println(("2^1 = ") + (b = 2*a));
+        } 
+        
+        else {
+            System.out.println("Error");
+        }
+    }
+}
